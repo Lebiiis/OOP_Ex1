61b7c89388df3b5e5a35fe1e750e47762a553da6
Niki Leppänen <niki.leppanen@edu.turkuamk.fi>
2021-01-15 16:49:42 +0200

    Added Exercise 1 Task 4

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import random
 
-"""Task 1"""
-print("hello")
+#"""Task 1"""
+#print("hello")
 
-"""Task 2"""
+#"""Task 2"""
 
-numlst = []
-for i in range(10):
-    num = int(input("Choose a number: "))
-    numlst.append(num)
+#numlst = []
+#for i in range(10):
+#    num = int(input("Choose a number: "))
+#    numlst.append(num)
 
-strlst = []
-for i in range(10):
-    string = input("Write something: ")
-    strlst.append(string)
+#strlst = []
+#for i in range(10):
+#    string = input("Write something: ")
+#    strlst.append(string)
 
-print(numlst)
-print(strlst)
+#print(numlst)
+#print(strlst)
 
-for i in range(len(numlst)-1):
-    numlst[i] = random.randint(0, 9999)
+#for i in range(len(numlst)-1):
+#    numlst[i] = random.randint(0, 9999)
 
-print(numlst)
+#print(numlst)
 
-"""Task 3"""
+#"""Task 3"""
 
-numlst.sort()
-strlst.sort()
+#numlst.sort()
+#strlst.sort()
 
-print("Sorted number list: ", numlst)
-print("String list in alphabetical order: ", strlst)
+#print("Sorted number list: ", numlst)
+#print("String list in alphabetical order: ", strlst)
+
+"""Task 4"""
+
+
+def askuser():
+    print("Write a number, 0 will stop the loop.")
+    negative = 0
+    while True:
+        number = int(input())
+        if number == 0:
+            print("Amount of negative inputs: ", negative)
+            return
+        elif number < 0:
+            negative += 1
+            print(number)
+        else:
+            print(number)
+
+
+askuser()
