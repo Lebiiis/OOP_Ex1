611115534d9b3d64d53d02b9af7ba914ce173b42
Niki Leppänen <niki.leppanen@edu.turkuamk.fi>
2021-01-15 16:03:31 +0200

    Added Exercise 1 Task 2

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import random
+
 """Exercise 1"""
 print("hello")
+
+"""Exercise 2"""
+
+numlst = []
+for i in range(10):
+    num = int(input("Choose a number: "))
+    numlst.append(num)
+
+strlst = []
+for i in range(10):
+    string = input("Write something: ")
+    strlst.append(string)
+
+print(numlst)
+print(strlst)
+
+for i in range(len(numlst)-1):
+    numlst[i] = random.randint(0, 9999)
+
+print(numlst)
