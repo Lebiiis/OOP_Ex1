f379145d7a221482e3f6105c4db8994e91095727
Niki Leppänen <niki.leppanen@edu.turkuamk.fi>
2021-01-19 16:27:22 +0200

    Added directory for Exercise 2 and Task 3

---
+++ Ex2/Ex2_3.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the amount of accepted exercises: "))
+
+if num == 9:
+    print("Your grade is 1")
+elif num == 10:
+    print("Your grade is 2")
+elif num == 11:
+    print("Your grade is 3")
+elif num == 12:
+    print("Your grade is 4")
+elif num == 13:
+    print("Your grade is 5")
+else:
+    print("You failed the course!")
